e718c7b4bf440475548aaee3fd609abc51458492
FanzeeApp <fanzeepubgm@gmail.com>
2024-11-08 06:06:36 +0500

    12345

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const mongoose = require("mongoose");
+
 mongoose
-  .connect("mongodb://localhost:27017/your-database-name", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB ga muvaffaqiyatli ulanildi"))
+  .connect(
+    "mongodb+srv://fanzeeoff:king2007root@cluster.ii4il.mongodb.net/?retryWrites=true&w=majority&appName=Cluster",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB ga muvaffaqiyatli ulandi"))
   .catch((error) => console.error("MongoDB ga ulanishda xato:", error));
 
 const Product = mongoose.model(
@@ -38,12 +43,10 @@ app.post("/products", async (req, res) => {
     });
 
     await newProduct.save();
-    res
-      .status(201)
-      .json({
-        message: "Mahsulot muvaffaqiyatli qo'shildi",
-        product: newProduct,
-      });
+    res.status(201).json({
+      message: "Mahsulot muvaffaqiyatli qo'shildi",
+      product: newProduct,
+    });
   } catch (error) {
     console.error("Mahsulot qo'shishda xato:", error);
     res.status(500).json({ error: "Server xatosi" });
